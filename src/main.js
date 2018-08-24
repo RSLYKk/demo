6663e8d701f67f84bfa25ffab91ee2389ff91f0e
rschenweiting <519933364@qq.com>
2018-08-24 09:54:48 +0800

    上半部结构以及动画完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+// 导入路由
+import VueRouter from "vue-router";
+import App from './App.vue';
+import Index from './components/index.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+Vue.use(VueRouter);
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+let routes=[
+  //重定项
+  {
+    path:'/',
+    redirect:"/index"
+  },
+  {
+    path:'/index',
+    component:Index
+  }
+];
 
+//实例化路由对象
+let router=new VueRouter({
+  routes
+})
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
